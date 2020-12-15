59d621c883b3756788fa9b5f9bc54867c3cc6519
alper111 <ahmetoglu.alper@gmail.com>
2020-12-15 11:00:35 +0300

    add option for clustered version

--- save_cat.py
+++ save_cat.py
@@ -1,10 +1,12 @@
 import os
 import argparse
+
 import torch
 import yaml
-from models import EffectRegressorMLP
-import data
 
+import data
+import utils
+from models import EffectRegressorMLP
 
 parser = argparse.ArgumentParser("Save categories.")
 parser.add_argument("-opts", help="option file", type=str, required=True)
@@ -33,13 +35,27 @@ for i in range(B):
 
 with torch.no_grad():
     category1 = model.encoder1(Y.to(device))
+if "discrete" in opts and not opts["discrete"]:
+    centroids, indices, _, _ = utils.kmeans(category1, k=2**opts["code1_dim"])
+    torch.save(centroids.cpu(), os.path.join(opts["save"], "centroids_1.pth"))
+    category1 = torch.zeros(indices.shape[0], opts["code1_dim"])
+    for i, idx in enumerate(indices):
+        category1[i] = torch.tensor(utils.decimal_to_binary(idx, length=opts["code1_dim"]))
 category1 = category1.int()
 
 left_img = Y.repeat_interleave(B, 0)
 right_img = Y.repeat(B, 1, 1, 1)
 concat = torch.cat([left_img, right_img], dim=1)
 
-category2 = model.encoder2(concat.to(device)).int()
+category2 = model.encoder2(concat.to(device))
+if "discrete" in opts and not opts["discrete"]:
+    centroids, indices, _, _ = utils.kmeans(category2, k=2**opts["code2_dim"])
+    torch.save(centroids.cpu(), os.path.join(opts["save"], "centroids_2.pth"))
+    category2 = torch.zeros(indices.shape[0], opts["code2_dim"])
+    for i, idx in enumerate(indices):
+        category2[i] = torch.tensor(utils.decimal_to_binary(idx, length=opts["code2_dim"]))
+category2 = category2.int()
+
 left_cat = category1.repeat_interleave(B, 0)
 right_cat = category1.repeat(B, 1)
 category_all = torch.cat([left_cat, right_cat, category2], dim=-1)
