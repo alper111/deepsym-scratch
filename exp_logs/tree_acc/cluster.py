0e717f81793dd399a2ec995b983e351af575f822
alper111 <ahmetoglu.alper@gmail.com>
2020-12-19 13:17:25 +0300

    add exp results

---
+++ exp_logs/tree_acc/cluster.py
@@ -0,0 +1,17 @@
+import torch
+
+x = torch.tensor([
+    83.8,
+    72.6,
+    78.5,
+    79.7,
+    78.0,
+    76.8,
+    75.8,
+    75.7,
+    73.9,
+    72.9
+])
+
+print(x.mean())
+print(x.std())
---
+++ exp_logs/tree_acc/deepsym.py
@@ -0,0 +1,17 @@
+import torch
+
+x = torch.tensor([
+    69.4,
+    76.5,
+    79.8,
+    73.8,
+    78.7,
+    79.4,
+    78.1,
+    67.2,
+    77.0,
+    69.8
+])
+
+print(x.mean())
+print(x.std())
