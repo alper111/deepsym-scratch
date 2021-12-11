afb65b6c694aa80c212a3e6ed431d210c6a057df
alper111 <ahmetoglu.alper@gmail.com>
2021-12-11 11:28:01 +0300

    original base collection

--- exp3_collect.py
+++ exp3_collect.py
@@ -1,7 +1,7 @@
 import torch
 from envs import TilePuzzleMNIST
 
-NUM_TRAJ = 500000
+NUM_TRAJ = 100000
 EPISODE_LENGTH = 1
 SIZE = 3
 NUM_ACTIONS = 4
@@ -12,7 +12,7 @@ action = torch.zeros(NUM_TRAJ, EPISODE_LENGTH, NUM_ACTIONS, dtype=torch.uint8)
 eye = torch.eye(NUM_ACTIONS, dtype=torch.uint8)
 
 for traj in range(NUM_TRAJ):
-    env = TilePuzzleMNIST(size=SIZE, permutation="replacement")
+    env = TilePuzzleMNIST(size=SIZE)
     for t in range(EPISODE_LENGTH):
         s_before = env.state()
         action_index = torch.randint(0, NUM_ACTIONS, ())
