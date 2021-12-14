951eba6184f5d4c3ba80b2eafbbf2b95a297ed27
alper111 <ahmetoglu.alper@gmail.com>
2021-12-14 18:36:15 +0300

    add three step tests

--- exp3_problem.py
+++ exp3_problem.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+from copy import deepcopy
 
 import torch
 import matplotlib.pyplot as plt
@@ -19,8 +20,8 @@ if os.path.exists(save_name):
 
 BN = True
 NUM_ACTIONS = 4
-NUM_BITS = 16
-SIZE = 5
+NUM_BITS = 13
+SIZE = 3
 
 encoder = torch.nn.Sequential(
     blocks.ConvBlock(in_channels=1, out_channels=64, kernel_size=4, stride=2, padding=1, batch_norm=BN),
@@ -33,15 +34,15 @@ encoder = torch.nn.Sequential(
 )
 
 # 13-bit or 14-bit, 3x3
-# decoder = torch.nn.Sequential(
-#     blocks.MLP([NUM_BITS+NUM_ACTIONS, 512]),
-#     blocks.Reshape([-1, 512, 1, 1]),
-#     blocks.ConvTransposeBlock(in_channels=512, out_channels=256, kernel_size=5, stride=1, padding=0, batch_norm=BN),
-#     blocks.ConvTransposeBlock(in_channels=256, out_channels=128, kernel_size=4, stride=2, padding=1, batch_norm=BN),
-#     blocks.ConvTransposeBlock(in_channels=128, out_channels=64, kernel_size=4, stride=2, padding=1, batch_norm=BN),
-#     blocks.ConvTransposeBlock(in_channels=64, out_channels=32, kernel_size=4, stride=2, padding=0, batch_norm=BN),
-#     torch.nn.ConvTranspose2d(in_channels=32, out_channels=1, kernel_size=4, stride=2, padding=1)
-# )
+decoder = torch.nn.Sequential(
+    blocks.MLP([NUM_BITS+NUM_ACTIONS, 512]),
+    blocks.Reshape([-1, 512, 1, 1]),
+    blocks.ConvTransposeBlock(in_channels=512, out_channels=256, kernel_size=5, stride=1, padding=0, batch_norm=BN),
+    blocks.ConvTransposeBlock(in_channels=256, out_channels=128, kernel_size=4, stride=2, padding=1, batch_norm=BN),
+    blocks.ConvTransposeBlock(in_channels=128, out_channels=64, kernel_size=4, stride=2, padding=1, batch_norm=BN),
+    blocks.ConvTransposeBlock(in_channels=64, out_channels=32, kernel_size=4, stride=2, padding=0, batch_norm=BN),
+    torch.nn.ConvTranspose2d(in_channels=32, out_channels=1, kernel_size=4, stride=2, padding=1)
+)
 
 # 15-bit, 4x4
 # decoder = torch.nn.Sequential(
@@ -55,15 +56,15 @@ encoder = torch.nn.Sequential(
 # )
 
 # 16-bit, 5x5
-decoder = torch.nn.Sequential(
-    blocks.MLP([NUM_BITS+NUM_ACTIONS, 512]),
-    blocks.Reshape([-1, 512, 1, 1]),
-    blocks.ConvTransposeBlock(in_channels=512, out_channels=256, kernel_size=8, stride=1, padding=0, batch_norm=BN),
-    blocks.ConvTransposeBlock(in_channels=256, out_channels=128, kernel_size=4, stride=2, padding=1, batch_norm=BN),
-    blocks.ConvTransposeBlock(in_channels=128, out_channels=64, kernel_size=4, stride=2, padding=0, batch_norm=BN),
-    blocks.ConvTransposeBlock(in_channels=64, out_channels=32, kernel_size=4, stride=2, padding=0, batch_norm=BN),
-    torch.nn.ConvTranspose2d(in_channels=32, out_channels=1, kernel_size=4, stride=2, padding=1)
-)
+# decoder = torch.nn.Sequential(
+#     blocks.MLP([NUM_BITS+NUM_ACTIONS, 512]),
+#     blocks.Reshape([-1, 512, 1, 1]),
+#     blocks.ConvTransposeBlock(in_channels=512, out_channels=256, kernel_size=8, stride=1, padding=0, batch_norm=BN),
+#     blocks.ConvTransposeBlock(in_channels=256, out_channels=128, kernel_size=4, stride=2, padding=1, batch_norm=BN),
+#     blocks.ConvTransposeBlock(in_channels=128, out_channels=64, kernel_size=4, stride=2, padding=0, batch_norm=BN),
+#     blocks.ConvTransposeBlock(in_channels=64, out_channels=32, kernel_size=4, stride=2, padding=0, batch_norm=BN),
+#     torch.nn.ConvTranspose2d(in_channels=32, out_channels=1, kernel_size=4, stride=2, padding=1)
+# )
 
 model = DeepSymbolGenerator(encoder=encoder, decoder=decoder, subnetworks=[],
                             device="cpu", lr=1e-4, path=args.s, coeff=9.0)
@@ -76,18 +77,25 @@ for p in model.encoder.parameters():
 for p in model.decoder.parameters():
     p.requires_grad = False
 
-env = TilePuzzleMNIST(size=SIZE)
+env = TilePuzzleMNIST(size=SIZE, permutation=torch.tensor([2, 4, 1, 6, 7, 3, 8, 0, 5]))
+new_env = deepcopy(env)
 x_init = env.state().unsqueeze(0)
+
+new_env.step(1)
+new_env.step(0)
+new_env.step(3)
+x_goal = new_env.state().unsqueeze(0)
 
 # generate goal encoding by randomly sampling goals
 # x_goal = env.random_goal_state().unsqueeze(0)
+# x_goal = env.goal_state().unsqueeze(0)
 # generate goal encoding by averaging goals
-r_idx = torch.randint(0, 9, ()).item()
-x_goal = torch.stack([env.avg_goal_state(r_idx) for i in range(1000)])
+# r_idx = torch.randint(0, 9, ()).item()
+# x_goal = torch.stack([env.avg_goal_state(r_idx) for i in range(1000)])
 
 z_init = model.encode(x_init).round().int()[0]
-# z_goal = model.encode(x_goal).round().int()[0]
-z_goal = model.encode(x_goal).mean(dim=0)
+z_goal = model.encode(x_goal).round().int()[0]
+# z_goal = model.encode(x_goal).mean(dim=0)
 tau = 0.05
 print(z_init)
 print(["%.3f" % i for i in z_goal])
@@ -115,7 +123,7 @@ print("))\n)", file=open(save_name, "a"))
 
 
 planner = mGPT(rounds=1000, max_time=600, heuristic="ff")
-domain_file = os.path.join(args.s, "ddomain_mnist.pddl")
+domain_file = os.path.join(args.s, "pdomain_mnist.pddl")
 problem_file = os.path.join(args.s, "problem_mnist.pddl")
 valid, output = planner.find_plan(domain_file, problem_file)
 if valid:
